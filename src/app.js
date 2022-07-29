f3c7a0034736761d2f254badc4ee08cef43f1d4a
shlomi <shl.lev.ks@gmail.com>
2022-07-30 00:27:27 +0300

    start boiler

--- src/app.js
+++ src/app.js
@@ -1,4 +1,18 @@
-import $ from 'jquery';
-import Rx from 'rxjs/Rx';
+import $ from "jquery";
+import Rx from "rxjs/Rx";
 
-console.log('RxJS Boiler Running...');
+// const btn = $("#btn");
+
+// const btnStream$ = Rx.Observable.fromEvent("btn", "click");
+
+// btnStream$.subscribe(
+//   function (e) {
+//     console.log("Clicked");
+//   },
+//   function (err) {
+//     console.log(err);
+//   },
+//   function () {
+//     console.log("Completed");
+//   }
+// );
